a2193ec38862bf551cdaeab3b7740210dbddb643
mateus <mateusc.rossetto@hotmail.com>
2022-09-20 13:45:16 -0300

    feat: add route active card

--- src/main/java/com/valex/controller/CardController.java
+++ src/main/java/com/valex/controller/CardController.java
@@ -7,11 +7,15 @@ import com.valex.service.CardService;
 import java.util.List;
 import javax.persistence.PostUpdate;
 import javax.validation.Valid;
+import org.apache.coyote.Request;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,9 +40,15 @@ public class CardController {
     this.cardService.create(cardDto);
   }
 
-  @PostUpdate
-  @ResponseStatus (code = HttpStatus.GONE)
-  public void activeCard (@RequestBody @Valid ActivateCardDto activateCardDto) {
+  @PutMapping ("/activate/{id}")
+  @ResponseStatus (code = HttpStatus.NO_CONTENT)
+  public void activeCard (
+      @RequestBody @Valid ActivateCardDto activateCardDto,
+      @PathVariable Long id
+  ) {
+    //Pegar id passar para a service ver se o cartão existe
+    //Salvar o CVV encoded e password
+    //mudar status para active e setar a senha
   }
 
 }
--- src/main/java/com/valex/domain/dto/ActivateCardDto.java
+++ src/main/java/com/valex/domain/dto/ActivateCardDto.java
@@ -1,5 +1,15 @@
 package com.valex.domain.dto;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import lombok.Getter;
+
+@Getter
 public class ActivateCardDto {
+
+  @NotNull @NotEmpty
+  @Size (min = 4, max = 4)
+  private String password;
 
 }
