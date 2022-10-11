9f24d5a8d17ddf0e4edd4047cd66e45bf80d614f
mateus <mateusc.rossetto@hotmail.com>
2022-10-11 15:21:00 -0300

    feat: implement user controller unit test delete

--- src/test/java/com/valex/controller/UserControllerUnitTest.java
+++ src/test/java/com/valex/controller/UserControllerUnitTest.java
@@ -1,10 +1,12 @@
 package com.valex.controller;
 
 import static com.valex.domain.mother.UserMother.*;
+import static com.valex.domain.mother.UserMother.getUserResponse;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -15,6 +17,7 @@ import com.valex.domain.dto.UserDto;
 import com.valex.domain.mapper.UserMapper;
 import com.valex.domain.request.UserRequest;
 import com.valex.domain.response.UserResponse;
+import com.valex.repository.UserRepository;
 import com.valex.service.UserService;
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -27,6 +30,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 
 @SpringBootTest
@@ -80,7 +84,28 @@ public class UserControllerUnitTest {
 
     mvc.perform(get(BASE_URL))
         .andExpect(status().isOk())
-        .andExpect(jsonPath("$").isArray());
+        .andExpect(jsonPath("$").isArray())
+        .andExpect(jsonPath("$[0].id").value(userResponse.getId()))
+        .andExpect(jsonPath("$[0].name").value(userResponse.getName()))
+        .andExpect(jsonPath("$[0].email").value(userResponse.getEmail()))
+        .andExpect(jsonPath("$[0].cpf").value(userResponse.getCpf()))
+        .andExpect(jsonPath("$[0].password").doesNotHaveJsonPath())
+        .andExpect(jsonPath("$[1]").doesNotHaveJsonPath());
+
+
+    verify(userService).findAll();
+    verify(userMapper).dtoToResponse(anyList());
+  }
+
+  @Test
+  @WithMockUser
+  void givenDeleteUserRequestThenReturnStatusNoContent () throws Exception {
+    UserDto userDto = getUserDto();
+
+    String URL = BASE_URL + "/" + userDto.getId();
+    mvc.perform(delete(URL))
+        .andExpect(status().isNoContent())
+        .andExpect(jsonPath("$").doesNotHaveJsonPath());
   }
 
 }
